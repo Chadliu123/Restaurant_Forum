5fddcff7f6b9ee94475b736e4bd176a6eafe8362
Chad <xmen11034@gmail.com>
2018-01-09 22:52:42 +0800

    count favorites size in favorite/unfavorite action

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -23,4 +23,9 @@ class Restaurant < ApplicationRecord
     self.liked_users.include?(user)
   end
 
+  def count_favorites
+    self.favorited_count = self.favorites.size
+    self.save
+  end
+
 end
