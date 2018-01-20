8de48d53c2eac929afd1dd76ca16177d20d8191d
Chad <xmen11034@gmail.com>
2018-01-20 11:58:28 +0800

    setup routes for follow/unfollow action

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
     get  :ranking, :on => :collection
   end
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :followships, only: [:create, :destroy]
 
   namespace :admin do
     resources :restaurants
