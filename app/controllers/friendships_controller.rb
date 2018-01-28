35ca5a5837db728996bd5f5b061f3267c5435df8
Chad <xmen11034@gmail.com>
2018-01-28 18:24:58 +0800

    add create action for friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,14 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    @friendship = current_user.friendships.build(friending_id: params[:friending_id])
+    if @friendship.save
+      flash[:notice] = "對#{User.find_by(id: params[:friending_id]).name}送出交友邀請！"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
